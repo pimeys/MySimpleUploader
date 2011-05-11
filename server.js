737d23b667cc2e563aac394f1c877412d4200f65
Julius de Bruijn <pimeys@gmail.com>
2011-05-12 02:34:21 +0300

    Added some events for file stream.
    It still doesn't work, but at least something is working. Now have..to..sleep...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express.createServer();
 var pub = __dirname + '/public';
 var multipart = require('./lib/multipart-stack/');
@@ -21,10 +22,47 @@ app.post('/', function(req, res) {
   var parsed = multipart.parseContentType(req.headers['content-type']);
   if (parsed.type == 'multipart') {
     var parser = new multipart.Parser(req, parsed.boundary);
+    var fileName = null;
+    var fileStream = null;
+    var headers = null;
+
     parser.on('part', function(part) {
-      part.on('headers', function(headers) {
-	console.log(headers);
+      part.on('headers', function(head) {
+	console.log(head);
       });
+
+      debugger;
+
+      console.log("Started part, name = " + part.name + ", filename = " + part.filename);
+      fileName = "./public/uploads/" + part.filename;
+      fileStream = fs.createWriteStream(fileName);
+
+      fileStream.addListener("error", function(err) {
+	console.log("Got error while writing to file '" + fileName + "': ", err);
+      });
+      
+      fileStream.addListener("drain", function() {
+	req.resume();
+      });
+
+      part.data = function(chunk) {
+	req.pause();
+	console.log("Writing chunk");
+	fileStream.write(chunk, "binary");
+      };
+
+      part.end = function() {
+	fileStream.addListener("drain", function() {
+	  fileStream.end();
+	
+	  console.log("Upload complete!");
+
+	  res.sendHeader(200, { 'Content-Type': 'text/plain' });
+	  res.write("Thank you!");
+	  res.end();
+	  sys.puts("\n=> DONE");
+	});
+      };
     });
   }
 });
